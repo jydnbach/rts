197e3498671a40ef2f5f83fa91df365d20631a6e
jydnbach <jaydenkim95@gmail.com>
2024-07-28 08:42:55 +0900

    prop system with typescript color prop

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,3 +1,7 @@
-export const Child = () => {
-  return <div>Hi</div>;
+interface ChildProps {
+  color: string;
+}
+
+export const Child = ({ color }: ChildProps) => {
+  return <div>{color}</div>;
 };
