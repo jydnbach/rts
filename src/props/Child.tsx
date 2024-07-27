7d4e874dbed861719077a09717a48208d448c56c
jydnbach <jaydenkim95@gmail.com>
2024-07-28 08:49:10 +0900

    explicit component type annotations

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -5,3 +5,8 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>{color}</div>;
 };
+
+// ReactFC: react function component
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
