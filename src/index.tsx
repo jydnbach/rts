1e0fa34b46dc26c37f8ea5a909eb87bfb48e31fd
jydnbach <jaydenkim95@gmail.com>
2024-07-28 10:15:51 +0900

    input focus on load

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
 import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
   return (
     <div>
-      <GuestList />
+      {/* <GuestList /> */}
+      {/* <UserSearch /> */}
+      {/* <EventComponent /> */}
+      {/* ref */}
       <UserSearch />
-      <EventComponent />
     </div>
   );
 };
