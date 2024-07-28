16e69d4ea165a4cd07af42327f8d16478b6a9c1f
jydnbach <jaydenkim95@gmail.com>
2024-07-28 09:14:30 +0900

    state with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom';
+import GuestList from './state/GuestList';
 
 const App = () => {
   return (
     <div>
-      <h1>hi</h1>
+      <GuestList />
     </div>
   );
 };
