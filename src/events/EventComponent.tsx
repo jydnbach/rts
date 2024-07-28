8db41186de759dc3cf48c6df9cfb50c46c1cc5ed
jydnbach <jaydenkim95@gmail.com>
2024-07-28 10:04:38 +0900

    handling drag events

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -3,9 +3,17 @@ const EventComponent: React.FC = () => {
     console.log(e);
   };
 
+  // mouse over the target event in JSX to check type
+  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log('Im being dragged');
+  };
+
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me
+      </div>
     </div>
   );
 };
